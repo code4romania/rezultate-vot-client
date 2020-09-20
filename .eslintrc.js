f2d551efad64fe80b13016ea73e8fc9c58cb81ba
Marius Petcu <dapetcu21@gmail.com>
2020-09-20 19:38:12 +0300

    Format eslintrc and disable misbehaving rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,43 @@
 module.exports = {
-    extends: [
-      'airbnb-typescript',
-      'airbnb/hooks',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:jest/recommended',
-      'prettier',
-      'prettier/react',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended'
-    ],
-    plugins: ['react', '@typescript-eslint', 'jest'],
-    env: {
-      browser: true,
-      es6: true,
-      jest: true,
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "jest"],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    ecmaVersion: 2018,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "linebreak-style": "off",
+    "import/prefer-default-export": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
       },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      project: './tsconfig.json',
-    },
-    rules: {
-      'linebreak-style': 'off',
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto',
-        },
-      ],
-      '@typescript-eslint/camelcase': 'off', // Due to a bug
-    },
-  };
+    ],
+    "@typescript-eslint/camelcase": "off", // Due to a bug
+    "react/prop-types": "off",
+  },
+};
