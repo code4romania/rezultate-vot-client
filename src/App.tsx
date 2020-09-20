e38187788ee2a01f4e5d0929db3a32dd9cdfba79
Marius Petcu <dapetcu21@gmail.com>
2020-09-20 18:33:55 +0300

    Add Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { ElectionScopePicker, useElectionScopePickerApi, makeElectionApi, ElectionScopeIncomplete } from "@code4ro/reusable-components";
+import {
+  ElectionScopePicker,
+  useElectionScopePickerApi,
+  makeElectionApi,
+  ElectionScopeIncomplete,
+} from "@code4ro/reusable-components";
 
 const electionApi = makeElectionApi();
 
-function App() {
+const App: React.FC = () => {
   const [scope, setScope] = useState<ElectionScopeIncomplete>({ type: "national" });
   const apiData = useElectionScopePickerApi(electionApi, scope);
-  return (
-    <ElectionScopePicker value={scope} onChange={setScope} apiData={apiData} />
-  );
-}
+  return <ElectionScopePicker value={scope} onChange={setScope} apiData={apiData} />;
+};
 
 export default App;
