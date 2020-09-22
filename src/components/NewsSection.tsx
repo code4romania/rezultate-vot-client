5d8663ca78acca18cc7864d6797a85e0b8b09bb9
Marius Petcu <dapetcu21@gmail.com>
2020-09-23 00:45:53 +0300

    Fix linter warnings

--- src/components/NewsSection.tsx
+++ src/components/NewsSection.tsx
@@ -1,4 +1,4 @@
-import { BodyLarge, DivBodyLarge, ElectionNewsFeed, ElectionNewsSection, Heading2 } from "@code4ro/reusable-components";
+import { DivBodyLarge, ElectionNewsFeed, ElectionNewsSection, Heading2 } from "@code4ro/reusable-components";
 import React, { FC } from "react";
 import { Separator } from "./Separator";
 import classes from "./NewsSection.module.scss";
@@ -11,7 +11,7 @@ export const NewsSection: FC<{ feed: ElectionNewsFeed }> = ({ feed }) => {
         Live newsfeed
       </Heading2>
       <Separator className={classes.separator} />
-      <DivBodyLarge className={classes.label} >
+      <DivBodyLarge className={classes.label}>
         Aici vezi ultimele știri și informații relevante acestei alegeri, culese din surse de încredere de către echipa
         Code4Romania.
       </DivBodyLarge>
