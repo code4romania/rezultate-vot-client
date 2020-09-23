71c855cc317e77a774822f5f2e4209d56f1d1538
Marius Petcu <dapetcu21@gmail.com>
2020-09-23 21:42:49 +0300

    Update reusable-components with overay URL fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { makeElectionApi, HereMapsAPIKeyProvider } from "@code4ro/reusable-components";
+import {
+  makeElectionApi,
+  HereMapsAPIKeyProvider,
+  ElectionMapOverlayURLContext,
+  electionMapOverlayUrl,
+} from "@code4ro/reusable-components";
 import { mockElectionAPI } from "@code4ro/reusable-components/dist/es/mocks";
 import { ElectionAPIContext } from "./ElectionAPIContext";
 import { BallotListProvider } from "./BallotListProvider";
@@ -18,13 +23,17 @@ const electionApi = process.env.REACT_APP_ELECTION_API_MOCKS
 export const App: React.FC = () => {
   return (
     <Router>
-      <HereMapsAPIKeyProvider value={process.env.REACT_APP_HEREMAPS_API_KEY || ""}>
-        <ElectionAPIContext.Provider value={electionApi}>
-          <BallotListProvider>
-            <AppLayout />
-          </BallotListProvider>
-        </ElectionAPIContext.Provider>
-      </HereMapsAPIKeyProvider>
+      <ElectionMapOverlayURLContext.Provider
+        value={process.env.REACT_APP_ELECTION_MAP_OVERLAY_URL || electionMapOverlayUrl}
+      >
+        <HereMapsAPIKeyProvider value={process.env.REACT_APP_HEREMAPS_API_KEY || ""}>
+          <ElectionAPIContext.Provider value={electionApi}>
+            <BallotListProvider>
+              <AppLayout />
+            </BallotListProvider>
+          </ElectionAPIContext.Provider>
+        </HereMapsAPIKeyProvider>
+      </ElectionMapOverlayURLContext.Provider>
     </Router>
   );
 };
