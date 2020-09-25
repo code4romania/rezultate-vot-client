7daaf4d1b74a23a7740109dc32114d9d10cf4004
Marius Petcu <dapetcu21@gmail.com>
2020-09-25 03:44:57 +0300

    Include scope query params in embed link

--- src/components/EmbedButton.tsx
+++ src/components/EmbedButton.tsx
@@ -1,21 +1,25 @@
-import * as React from "react";
+import { electionScopeIsComplete } from "@code4ro/reusable-components";
+import React, { useCallback, useMemo } from "react";
+import { useLocation } from "react-router-dom";
 
 import embedImage from "../assets/embed.png";
+import { prependQuestionMark, scopeFromSearch, searchFromScope } from "../functions/urlState";
 import classes from "./EmbedButton.module.scss";
 import { useToast } from "./toast/withToastProvider";
 
 export const EmbedButton: React.FC<{ path: string }> = ({ path }) => {
   const toast = useToast();
-  const footerHeight = 75;
-  const getEmbeddableCode = `<iframe
-            scrolling="no"
-            src="${window.location.origin.toString()}/embed/${path}"
-            width="1000"
-            height="${1000 + footerHeight}"></iframe>`;
+  const location = useLocation();
+  const scope = useMemo(() => electionScopeIsComplete(scopeFromSearch(location.search))?.complete, [location.search]);
 
-  const handleCopyEmbedCode = () => {
+  const onCopyEmbedCode = useCallback(() => {
+    if (!scope) return;
+
     const auxiliaryField = document.createElement("textarea");
-    auxiliaryField.innerText = getEmbeddableCode.replace(/\s+(?=\s)/g, "");
+    const url = new URL(`/embed/${path}`, window.location.origin);
+    url.search = prependQuestionMark(searchFromScope(scope));
+
+    auxiliaryField.innerText = `<iframe scrolling="no" width="100%" height="600" style="border:0" src="${url.href}"></iframe>`;
 
     document.body.appendChild(auxiliaryField);
     auxiliaryField.select();
@@ -23,15 +27,20 @@ export const EmbedButton: React.FC<{ path: string }> = ({ path }) => {
 
     auxiliaryField.remove();
     toast.add("Codul de embed a fost copiat în clipboard");
-  };
+  }, [scope, path, toast]);
 
   // if already embedded hide the button
-  if (window.location.pathname.indexOf("/embed") !== -1) {
+  if (location.pathname.indexOf("/embed") !== -1) {
+    return null;
+  }
+
+  // if the scope is incomplete, hide the button
+  if (!scope) {
     return null;
   }
 
   return (
-    <button type="button" title="Copiază cod de embed" className={classes.embedBtn} onClick={handleCopyEmbedCode}>
+    <button type="button" title="Copiază cod de embed" className={classes.embedBtn} onClick={onCopyEmbedCode}>
       <img src={embedImage} alt="embed" className="fab-icon" />
     </button>
   );
