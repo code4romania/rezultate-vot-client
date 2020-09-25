494dc5cb748ac1e7ad309b4499c0c40c81c8f61d
Surdu <neon-doubt@maskmail.net>
2020-09-25 21:11:17 +0300

    Added footer to the embed widget

--- src/components/EmbedWrapper.tsx
+++ src/components/EmbedWrapper.tsx
@@ -1,11 +1,18 @@
+import { DevelopedBy } from "@code4ro/taskforce-fe-components/dist";
+import "@code4ro/taskforce-fe-components/dist/index.css";
 import React, { ReactNode } from "react";
 import { withToastProvider } from "./toast/withToastProvider";
+import classes from "./EmbedWrapper.module.scss";
+import { ReactComponent as Logo } from "../assets/logo.svg";
 
 export const EmbedWrapper: React.FC<{ children: ReactNode }> = withToastProvider(
   ({ children }: { children: ReactNode }) => (
     <>
       {children}
-      {/* TODO: Add some fancy code4ro footer here */}
+      <div className={classes.footer}>
+        <Logo className={classes.logo} />
+        <DevelopedBy showPartners={false} />
+      </div>
     </>
   ),
 );
