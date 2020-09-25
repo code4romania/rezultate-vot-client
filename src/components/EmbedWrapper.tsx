6d8e572edca5de57a02ac764f7ad6f20345a4d35
Nicu <neon-doubt@maskmail.net>
2020-09-25 22:07:02 +0300

    Added footer to the embed widget (#38)
    * Added footer to the embed widget
    
    * Implemented custom footer

--- src/components/EmbedWrapper.tsx
+++ src/components/EmbedWrapper.tsx
@@ -1,11 +1,26 @@
 import React, { ReactNode } from "react";
 import { withToastProvider } from "./toast/withToastProvider";
+import classes from "./EmbedWrapper.module.scss";
+import { ReactComponent as ProjectLogo } from "../assets/logo.svg";
+import { ReactComponent as Code4Logo } from "../assets/code4romania.svg";
 
 export const EmbedWrapper: React.FC<{ children: ReactNode }> = withToastProvider(
   ({ children }: { children: ReactNode }) => (
     <>
       {children}
-      {/* TODO: Add some fancy code4ro footer here */}
+      <div className={classes.footer}>
+        <ProjectLogo />
+        <div className={classes.code4Logo}>
+          by
+          <a target="_blank" rel="noopener noreferrer" href="https://code4.ro">
+            <Code4Logo />
+          </a>
+        </div>
+
+        <a className={classes.button} target="_blank" rel="noopener noreferrer" href="https://code4.ro/ro/doneaza/">
+          Doneaza
+        </a>
+      </div>
     </>
   ),
 );
