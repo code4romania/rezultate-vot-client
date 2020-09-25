e586aca5be790a8aed7aad9262412d0f56f24a1a
Razvan Ciuca <razvann88@gmail.com>
2020-09-25 22:08:52 +0300

    fix(AboutPage):resolve linting issues

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -32,7 +32,7 @@ export const AboutPage: React.FC = () => (
           </a>
         </div>
         <div>
-          <img src={tfsgLogo} alt=""/>
+          <img src={tfsgLogo} alt="" />
         </div>
       </div>
       <p>Pe Rezultate Vot veți găsi:</p>
@@ -43,11 +43,11 @@ export const AboutPage: React.FC = () => (
         </li>
         <li>
           Informații din sistemul de monitorizare digitală a alegerilor - Monitorizare Vot - realizat de Code for
-          Romania
-          și utilizat de observatorii alegerilor
+          Romania și utilizat de observatorii alegerilor
         </li>
         <li>Istoricul electoral al României pentru toate rundele de alegeri începând cu anul 1992</li>
-        <li>Un flux live de comentarii și analize realizate de sociologi din marile centre universitare din România.
+        <li>
+          Un flux live de comentarii și analize realizate de sociologi din marile centre universitare din România.
         </li>
       </ul>
       <p>
@@ -61,9 +61,8 @@ export const AboutPage: React.FC = () => (
           </a>{" "}
           este aplicația web și mobilă dedicată observatorilor electorali independenți - prima aplicație de monitorizare
           electorală din România, fiind și cea mai utilizată de acest tip la nivel global. Lansată în 2016 aceasta a
-          fost
-          folosită la toate rundele electorale din ultimii patru ani și a fost preluată inclusiv în Polonia, în 2018, în
-          cadrul primelor alegeri monitorizate independent în statul polonez.
+          fost folosită la toate rundele electorale din ultimii patru ani și a fost preluată inclusiv în Polonia, în
+          2018, în cadrul primelor alegeri monitorizate independent în statul polonez.
         </li>
         <li>
           A doua aplicație din ecosistem este{" "}
@@ -90,7 +89,7 @@ export const AboutPage: React.FC = () => (
       <p>Lorem ipsum</p>
       <div className={classes.expertWrapper}>
         <div className={classes.expertImage}>
-          <img src={claudiuTufis} alt=""/>
+          <img src={claudiuTufis} alt="" />
         </div>
         <div className={classes.expertDescription}>
           <Heading3>Claudiu Tufiș</Heading3>
@@ -98,21 +97,19 @@ export const AboutPage: React.FC = () => (
             Conferențiar universitar în cadrul Facultății de Științe Politice din cadrul Universității din București.
             Doctor în științe politice al Pennsylvania State University. Înainte de doctorat am obținut o diplomă de
             master în sociologie la Central European University și o diplomă de licență în sociologie la Universitatea
-            din
-            București. Principalele sale arii de interes academic includ: cultură politică, comportament politic, studii
-            electorale, societate civilă, mișcări sociale, politici publice în domeniul învățământului superior,
+            din București. Principalele sale arii de interes academic includ: cultură politică, comportament politic,
+            studii electorale, societate civilă, mișcări sociale, politici publice în domeniul învățământului superior,
             metodologia cercetării sociale și analiza datelor sociale. Studiile sale electorale includ Alegerile pentru
             Parlamentul European (coord. cu Mircea Comșa și Andrei Gheorghiță, publicat la Polirom), Alegerile
             prezidențiale din România, 2009 (coord. cu Mircea Comșa și Andrei Gheorghiță, publicat la Presa Universitară
             Clujeană), precum și articole în jurnale precum European Politics and Society, Problems of Post-Communism,
-            sau
-            European Political Science Review.
+            sau European Political Science Review.
           </p>
         </div>
       </div>
       <div className={classes.expertWrapper}>
         <div className={classes.expertImage}>
-          <img src={alexandraIancu} alt=""/>
+          <img src={alexandraIancu} alt="" />
         </div>
         <div className={classes.expertDescription}>
           <Heading3>Alexandra Iancu</Heading3>
@@ -124,37 +121,35 @@ export const AboutPage: React.FC = () => (
             élitisme, apolitisme et informalité politique (Iași:Institutul European, 2019) ; Corruption et politique en
             Europe. Enjeux, réformes et controverses (coord. cu Silvia Marton, Paris: L’Harmattan, 2019 ; Party Members
             and Their Importance in Non-EU Countries (coord. cu Sergiu Gherghina, Sorina Soare, Londra: Routledge,
-            2018).
-            Domeniile sale de cercetare vizează: organizațiile partidelor politice, recrutarea elitelor și istoria
-            corupției politice (a denunțării corupției politice și a practicilor de patronaj politic) în Europa Centrală
-            și de Est. O axă de cercetare recent dezvoltată se referă la analiza discursurilor politice contestare la
-            adresa Uniunii europene în noile state membre.
+            2018). Domeniile sale de cercetare vizează: organizațiile partidelor politice, recrutarea elitelor și
+            istoria corupției politice (a denunțării corupției politice și a practicilor de patronaj politic) în Europa
+            Centrală și de Est. O axă de cercetare recent dezvoltată se referă la analiza discursurilor politice
+            contestare la adresa Uniunii europene în noile state membre.
           </p>
         </div>
       </div>
       <div className={classes.expertWrapper}>
         <div className={classes.expertImage}>
-          <img src={florinFesnic} alt=""/>
+          <img src={florinFesnic} alt="" />
         </div>
         <div className={classes.expertDescription}>
           <Heading3>Florin Feșnic</Heading3>
           <p>
             Cercetător la Centrul pentru Studierea Democraţiei din cadrul Departamentului de Ştiinţe Politice al UBB
-            Cluj.
-            Printre interesele sale de cercetare se numără educaţia civică, democratizarea, designul instituţional,
-            sociologia politică, comportamentul electoral, partidele şi sistemele de partide, determinanţii şi
-            consecinţele politice ale modernizării, paradoxurile politice şi scientometria. Printre publicaţiile sale
-            recente se numără „The Democratic-Liberal Party as a Successful Catch-All Party” (2011), în Ronald King şi
-            Paul Sum (editori), Romania under Băsescu, Lexington Books, şi „Can Civic Education Make a Difference for
-            Democracy? Hungary and Poland Compared” (Political Studies) şi “This time it’s different? Effects of the
-            Eurovision Debate on young citizens” (cu Jürgen Maier, Thorsten Faas, Carmen Greab, et al., Journal of
-            European Public Policy).
+            Cluj. Printre interesele sale de cercetare se numără educaţia civică, democratizarea, designul
+            instituţional, sociologia politică, comportamentul electoral, partidele şi sistemele de partide,
+            determinanţii şi consecinţele politice ale modernizării, paradoxurile politice şi scientometria. Printre
+            publicaţiile sale recente se numără „The Democratic-Liberal Party as a Successful Catch-All Party” (2011),
+            în Ronald King şi Paul Sum (editori), Romania under Băsescu, Lexington Books, şi „Can Civic Education Make a
+            Difference for Democracy? Hungary and Poland Compared” (Political Studies) şi “This time it’s different?
+            Effects of the Eurovision Debate on young citizens” (cu Jürgen Maier, Thorsten Faas, Carmen Greab, et al.,
+            Journal of European Public Policy).
           </p>
         </div>
       </div>
       <div className={classes.expertWrapper}>
         <div className={classes.expertImage}>
-          <img src={bogdanVoicu} alt=""/>
+          <img src={bogdanVoicu} alt="" />
         </div>
         <div className={classes.expertDescription}>
           <Heading3>Bogdan Voicu</Heading3>
@@ -164,20 +159,16 @@ export const AboutPage: React.FC = () => (
             academice în jurnale româneşti și internaţionale, cele mai cunoascute fiind European Sociological Review,
             Journal for Ethnic and Migration Studies, Current Sociology, Social Indicators Research. Între volumele
             publicate în ultimii ani se numără Penuria Pseudomodernă a Postcomunismului românesc (Expert Projects,
-            2004),
-            Satul românesc pe drumul spre Europa (volum coordonat alături de Mălina Voicu, Polirom, 2006), Valori ale
-            românilor: 1993-2006. O perspectivă sociologică (volum coordonat alături de Mălina Voicu, Institutul
-            European,
-            2007), Capital social în România începutului de Mileniu: Drumeţ în ţara celor fără de prieteni? (2010,
-            Lumen),
-            Gender, Family, and Adaptation of Migrants in Europe. A Life Course Perspective (2018, Palgrave; volum
-            coordonat alături de Ionela Vlase). Mai multe detalii sunt prezente pe pagina sa de Internet:
-            http://web.bogdanvoicu.ro.
+            2004), Satul românesc pe drumul spre Europa (volum coordonat alături de Mălina Voicu, Polirom, 2006), Valori
+            ale românilor: 1993-2006. O perspectivă sociologică (volum coordonat alături de Mălina Voicu, Institutul
+            European, 2007), Capital social în România începutului de Mileniu: Drumeţ în ţara celor fără de prieteni?
+            (2010, Lumen), Gender, Family, and Adaptation of Migrants in Europe. A Life Course Perspective (2018,
+            Palgrave; volum coordonat alături de Ionela Vlase). Mai multe detalii sunt prezente pe pagina sa de
+            Internet: http://web.bogdanvoicu.ro.
           </p>
         </div>
       </div>
-
     </DivBody>
-    <Footer/>
+    <Footer />
   </>
 );
