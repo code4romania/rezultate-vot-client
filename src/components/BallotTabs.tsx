5d5c5708ba4e55150c73e023f321024282a4fc44
Marius Petcu <dapetcu21@gmail.com>
2020-09-28 22:17:37 +0300

    Fix Primaria Capitalei tab active state

--- src/components/BallotTabs.tsx
+++ src/components/BallotTabs.tsx
@@ -1,41 +1,57 @@
 import { mergeClasses } from "@code4ro/reusable-components";
 import React, { PropsWithChildren, ReactNode } from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
+import { useCompleteScopeFromSearch } from "../functions/urlState";
 
 import classes from "./BallotTabs.module.scss";
 
-const TabNavLink = ({ children, to, search }: PropsWithChildren<{ to: string; search?: string }>) => (
-  <Route path={to}>
-    {({ match }) => (
-      <Link
-        to={(location) => {
-          const queryString = search || location.search;
-          return { ...location, pathname: to, search: queryString };
-        }}
-        className={mergeClasses(classes.navLink, match && classes.navLinkActive)}
-      >
-        {children}
-      </Link>
-    )}
-  </Route>
-);
+const TabNavLink = ({
+  children,
+  to,
+  search,
+  active,
+}: PropsWithChildren<{ to: string; search?: string; active?: boolean }>) => {
+  const match = useRouteMatch(to);
+  return (
+    <Link
+      to={(location) => {
+        const queryString = search || location.search;
+        return { ...location, pathname: to, search: queryString };
+      }}
+      className={mergeClasses(classes.navLink, match && (active == null || active) && classes.navLinkActive)}
+    >
+      {children}
+    </Link>
+  );
+};
 
 type Props = {
   ballotId: number;
   children: ReactNode;
   indicators?: ReactNode;
+  hasCapitalMayor?: boolean;
   mode?: string;
 };
 
-export const BallotTabs: React.FC<Props> = ({ ballotId, children, indicators, mode }) => {
+const bucharestCountyId = 12913;
+
+export const BallotTabs: React.FC<Props> = ({ ballotId, children, indicators, mode, hasCapitalMayor }) => {
   const baseUrl = `/${mode || "elections"}`;
+  const scope = useCompleteScopeFromSearch();
+  const routeMatchesCapital = scope?.type === "county" && scope.countyId === bucharestCountyId;
   return (
     <div className={classes.root}>
       <div className={classes.tabs}>
         <TabNavLink to={`${baseUrl}/${ballotId}/turnout`}>Prezența la vot</TabNavLink>
-        <TabNavLink to={`${baseUrl}/${ballotId}/results`}>Rezultate vot</TabNavLink>
-        {ballotId === 95 && (
-          <TabNavLink to={`${baseUrl}/${ballotId}/results`} search="?division=county&countyId=12913">
+        {(!hasCapitalMayor || !routeMatchesCapital) && (
+          <TabNavLink to={`${baseUrl}/${ballotId}/results`}>Rezultate vot</TabNavLink>
+        )}
+        {hasCapitalMayor && (
+          <TabNavLink
+            to={`${baseUrl}/${ballotId}/results`}
+            search={`?division=county&countyId=${bucharestCountyId}`}
+            active={routeMatchesCapital}
+          >
             Primarul Capitalei
           </TabNavLink>
         )}
