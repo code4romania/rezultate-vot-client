82d784568da48f1b40062cf9088fd2488de1c60b
Marius Petcu <dapetcu21@gmail.com>
2020-11-09 16:31:51 +0200

    Add single news card embed route

---
+++ src/components/NewsCardPage.tsx
@@ -0,0 +1,43 @@
+import { ElectionNewsCard } from "@code4ro/reusable-components";
+import React from "react";
+import { Helmet } from "react-helmet";
+import { NavLink } from "react-router-dom";
+import { useBallotFromRoute, useNewsItemIdFromRoute } from "../functions/urlState";
+
+import classes from "./NewsCardPage.module.scss";
+
+export const NewsCardPage: React.FC = () => {
+  const { data } = useBallotFromRoute();
+  const newsItemId = useNewsItemIdFromRoute();
+
+  if (!data) return null;
+
+  const newsItem = data?.electionNews?.find((n) => n.id === Number(newsItemId));
+
+  if (!newsItem) return null;
+
+  const feedLink = `${window.location?.origin}/feed/${data.meta?.ballotId}/news/${newsItemId}`;
+
+  return (
+    (data && data.electionNews && (
+      <div className={classes.cardWrapper}>
+        <Helmet>
+          <title>{newsItem.title}</title>
+          <meta name="description" content={newsItem.body} />
+          <meta property="og:title" content={newsItem.title || "Articol"} />
+        </Helmet>
+        <ElectionNewsCard key={newsItemId} news={newsItem} feedLink={feedLink} />
+        <NavLink
+          to={`/elections/${data.meta.ballotId}`}
+          className={classes.navLink}
+          activeClassName={classes.navLinkActive}
+        >
+          <button className={classes.button} type="button">
+            Înapoi la site
+          </button>
+        </NavLink>
+      </div>
+    )) ||
+    null
+  );
+};
