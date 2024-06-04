325e2fb47a61f48feb07272c6d55a2b4c66fb48c
Dinu-Iulian Popovici <dinu.iulian.popovici@gmail.com>
2024-06-04 13:55:38 +0300

    Apply linter

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,14 +10,18 @@ export const Banner: React.FC<Props> = ({ className }: Props) => {
   return (
     <div className={mergeClasses(classes.root, className)}>
       <p>
-        Rezultate Vot este una dintre zecile de soluții din {" "}
+        Rezultate Vot este una dintre zecile de soluții din{" "}
         <b>
-          <a href="https://www.code4.ro/ro/infrastructura-binelui" target="_blank">Infrastructura Binelui</a>
-        </b> construită pro-bono de Code for Romania. Ajută-ne să le ținem în viață și să le creștem. {" "}
+          <a href="https://www.code4.ro/ro/infrastructura-binelui" target="_blank" rel="noopener noreferrer">
+            Infrastructura Binelui
+          </a>
+        </b>{" "}
+        construită pro-bono de Code for Romania. Ajută-ne să le ținem în viață și să le creștem.{" "}
         <b>
           Trimite „PUTEM” prin SMS la <a href="sms://8864?body=PUTEM">8864</a>
         </b>{" "}
-        pentru a dona 4 euro lunar și ne poți ajuta să digitalizăm România construind sute de alte soluții la fel de utile.
+        pentru a dona 4 euro lunar și ne poți ajuta să digitalizăm România construind sute de alte soluții la fel de
+        utile.
       </p>
     </div>
   );
