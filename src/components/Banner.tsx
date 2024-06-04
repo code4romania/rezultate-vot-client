ecf508791ac47e7af4268c83d84de6ba33991812
Dinu-Iulian Popovici <dinu.iulian.popovici@gmail.com>
2024-06-04 13:49:49 +0300

    [2024 FE] Change main banner text
    Change main banner text to the new wording provided

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,15 +10,14 @@ export const Banner: React.FC<Props> = ({ className }: Props) => {
   return (
     <div className={mergeClasses(classes.root, className)}>
       <p>
-        Rezultate Vot este doar una dintre soluțiile proiectate și construite pro bono de voluntarii Code for Romania.{" "}
+        Rezultate Vot este una dintre zecile de soluții din {" "}
+        <b>
+          <a href="https://www.code4.ro/ro/infrastructura-binelui" target="_blank">Infrastructura Binelui</a>
+        </b> construită pro-bono de Code for Romania. Ajută-ne să le ținem în viață și să le creștem. {" "}
         <b>
           Trimite „PUTEM” prin SMS la <a href="sms://8864?body=PUTEM">8864</a>
         </b>{" "}
-        pentru a dona 4 euro lunar și ne poți ajuta să digitalizăm România construind sute de alte soluții la fel de
-        utile. Detalii pe{" "}
-        <a href="https://code4.ro/putem">
-          <b>code4.ro/putem</b>
-        </a>
+        pentru a dona 4 euro lunar și ne poți ajuta să digitalizăm România construind sute de alte soluții la fel de utile.
       </p>
     </div>
   );
