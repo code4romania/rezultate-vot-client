897661c39f0c40b90a99929d52fb9caf968ef06a
Dinu-Iulian Popovici <dinu.iulian.popovici@gmail.com>
2024-06-06 14:14:50 +0300

    Change embed branding strip
    Closes #153

--- src/components/EmbedWrapper.tsx
+++ src/components/EmbedWrapper.tsx
@@ -1,18 +1,22 @@
 import React, { ReactNode } from "react";
 import { withToastProvider } from "./toast/withToastProvider";
 import classes from "./EmbedWrapper.module.scss";
-import { Logo } from "./Logo";
+import { ReactComponent as ProjectLogo } from "../assets/logo.svg";
+import { ReactComponent as Code4Logo } from "../assets/code4romania.svg";
 
 export const EmbedWrapper: React.FC<{ children: ReactNode }> = withToastProvider(
   ({ children }: { children: ReactNode }) => (
     <>
       {children}
-      <div className={classes.footer}>
-        <Logo className={classes.logo} />
-        <div className={classes.buttonWrap}>
-          <a className={classes.button} target="_blank" rel="noopener noreferrer" href="https://code4.ro/ro/doneaza/">
-            Doneaza
-          </a>
+      <div className={classes.embeddedWidgetFooter}>
+        <div className={classes.embeddedWidgetContainer}>
+          <ProjectLogo className={classes.projectLogo} />
+          <Code4Logo className={classes.code4Logo} />
+          <div className={classes.buttonWrap}>
+            <a className={classes.button} target="_blank" rel="noopener noreferrer" href="https://code4.ro/ro/doneaza/">
+              Donează
+            </a>
+          </div>
         </div>
       </div>
     </>
