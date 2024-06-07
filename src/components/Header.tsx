e2302dbc8cf00daa8380d1541bf81f31225759f2
Dinu-Iulian Popovici <dinu.iulian.popovici@gmail.com>
2024-06-07 16:05:37 +0300

    Fix responsive issues (menu)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
 import classes from "./Header.module.scss";
 import { Logo } from "./Logo";
 import { ReactComponent as CommitGlobalLogo } from "../assets/commit-global-logo.svg";
+import { ReactComponent as HamburgerMenu } from "../assets/hamburger-button.svg";
 
 export const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const handleDocumentClick = (event: { target: any }) => {
+    // @ts-ignore
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("click", handleDocumentClick);
+    } else {
+      document.removeEventListener("click", handleDocumentClick);
+    }
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, [isOpen]);
+
   return (
     <>
       <div className={classes.brandingStrip}>
@@ -32,6 +53,42 @@ export const Header: React.FC = () => {
           </button>
         </a>
       </div>
+
+      <div className={`${classes.root} ${classes.hamburgerMenu}`}>
+        <Logo className={classes.logo} homeLink />
+        <div className={classes.separator} />
+        <div className={classes.dropdown}>
+          <button ref={dropdownRef} onClick={() => setIsOpen(!isOpen)} type="button" className={classes.dropdownButton}>
+            <HamburgerMenu />
+          </button>
+          <div className={`${classes.dropdownOptions} ${isOpen ? classes.open : classes.closed}`}>
+            <NavLink to="/elections" className={classes.navLink} activeClassName={classes.navLinkActive}>
+              Date alegeri
+            </NavLink>
+            <NavHashLink
+              to="/elections#live-newsfeed"
+              className={classes.navLink}
+              activeClassName={classes.navLinkActive}
+            >
+              Live newsfeed
+            </NavHashLink>
+            <NavLink to="/about" exact className={classes.navLink} activeClassName={classes.navLinkActive}>
+              Despre proiect
+            </NavLink>
+            <a
+              href="https://code4.ro/ro/doneaza/"
+              target="_blank"
+              rel="noreferrer noopener"
+              className={classes.navLink}
+            >
+              <button type="button" className={classes.donateButton}>
+                Donează
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+
       <div className={classes.contentPadding} />
     </>
   );
