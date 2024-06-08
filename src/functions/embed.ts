828f3ec275dee692e078ba9c8af20d263436d75c
Dinu-Iulian Popovici <dinu.iulian.popovici@gmail.com>
2024-06-08 13:47:33 +0300

    Update embed.ts

--- src/functions/embed.ts
+++ src/functions/embed.ts
@@ -7,7 +7,16 @@ export function makeEmbedCode(path: string, scope?: ElectionScope | null): strin
     url.search = prependQuestionMark(searchFromScope(scope));
   }
 
-  return `<script src="${window.location.origin}/iframeResizer.min.js" async></script><iframe scrolling="no" width="100%" height="600" style="border:0" src="${url.href}" onload="(window.iFrameResize || function () { var q = window.__iframeResizerQueue || []; window.__iframeResizerQueue = q; q.push(arguments); })({}, this)"></iframe>`;
+  return `<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CX2LL7DLB3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-CX2LL7DLB3');
+</script>
+<script src="${window.location.origin}/iframeResizer.min.js" async></script>
+<iframe scrolling="no" width="100%" height="600" style="border:0" src="${url.href}" onload="(window.iFrameResize || function () { var q = window.__iframeResizerQueue || []; window.__iframeResizerQueue = q; q.push(arguments); })({}, this)"></iframe>`;
 }
 
 export function copyEmbedCode(toast: { add: (s: string) => void }, code: string): void {
