ace9340384b2384115e3e8a4d5a1de2e8f8d514e
Dinu-Iulian Popovici <dinu.iulian.popovici@gmail.com>
2024-06-09 08:04:04 +0300

    Show Candidates in Turnout Page
    Added the logic to show candidates in the Turnout section depending on the type of election/candidate

--- src/components/TurnoutTab.tsx
+++ src/components/TurnoutTab.tsx
@@ -9,6 +9,8 @@ import {
 import React from "react";
 import { EmbedButton, EmbedButtonWrapper } from "./EmbedButton";
 import { Loader } from "./Loader";
+import { Separator } from "./Separator";
+import classes from "./ResultsTab.module.scss";
 
 type Props = {
   meta?: ElectionBallotMeta | null;
@@ -19,6 +21,15 @@ type Props = {
 };
 
 export const TurnoutTab: React.FC<Props> = ({ meta, ballot, scope, onScopeChange, loading }) => {
+  const partyLevelCondition =
+    (meta?.type === "county_council" && scope.type === "county") ||
+    (meta?.type === "local_council" && scope.type === "locality") ||
+    meta?.type === "european_parliament";
+
+  const individualLevelCondition =
+    (meta?.type === "mayor" && scope.type === "locality") ||
+    (meta?.type === "county_council_president" && scope.type === "county");
+
   return (
     <>
       <EmbedButtonWrapper>
@@ -34,6 +45,70 @@ export const TurnoutTab: React.FC<Props> = ({ meta, ballot, scope, onScopeChange
         {meta && <EmbedButton path={`${meta.ballotId}/observation`} />}
         {ballot?.observation && <ElectionObservationSection observation={ballot.observation} />}
       </EmbedButtonWrapper>
+
+      {/* This is a quick and dirty fix to show the candidates (or parties) in the turnout tab */}
+      {ballot?.results &&
+        ballot?.results?.candidates.length > 0 &&
+        meta &&
+        (individualLevelCondition ? (
+          <div className={classes.root}>
+            <Separator className={classes.tablesSeparator} />
+            <table>
+              <thead>
+                <tr>
+                  <th>Partid</th>
+                  <th>Candidat</th>
+                </tr>
+              </thead>
+              <tbody>
+                {ballot?.results.candidates.map((candidate) => (
+                  <tr key={candidate.name}>
+                    <td className={classes.logoTextCell}>
+                      <img
+                        width="50"
+                        src={candidate.partyLogo ? candidate.partyLogo : undefined}
+                        alt=""
+                        className={classes.partyLogo}
+                      />
+                      <span className={classes.name}>{candidate.partyName ? candidate.partyName : ""} </span>
+                    </td>
+                    <td>
+                      <strong>{candidate.name}</strong>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          partyLevelCondition && (
+            <div className={classes.root}>
+              <Separator className={classes.tablesSeparator} />
+              <table>
+                <thead>
+                  <tr>
+                    <th>Partid</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {ballot?.results.candidates.map((candidate) => (
+                    <tr key={candidate.name}>
+                      <td className={classes.logoTextCell}>
+                        <img
+                          width="50"
+                          src={candidate.partyLogo ? candidate.partyLogo : undefined}
+                          alt=""
+                          className={classes.partyLogo}
+                        />
+                        <span className={classes.name}>{candidate.name}</span>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )
+        ))}
     </>
   );
 };
